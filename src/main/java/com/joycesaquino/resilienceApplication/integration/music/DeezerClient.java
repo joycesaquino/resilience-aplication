bc56c0de03fcb9f58668603d948245a1f36d910a
joyce.aquino <joyce.aquino@b2wdigital.com>
2021-07-14 23:41:48 -0300

    [new] Método de fallback para a API do deezer

--- src/main/java/com/joycesaquino/resilienceApplication/integration/music/DeezerClient.java
+++ src/main/java/com/joycesaquino/resilienceApplication/integration/music/DeezerClient.java
@@ -1,11 +1,28 @@
 package com.joycesaquino.resilienceApplication.integration.music;
 
+import com.joycesaquino.resilienceApplication.model.Artist;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Optional;
 
 @Component
 public interface DeezerClient {
     @GetMapping("/album")
     Album getAlbum(Long id);
+
+    default Optional<Album> getAlbumFallback(Long id, Artist artist) {
+
+        return artist.getAlbums()
+                .entrySet()
+                .stream()
+                .filter(entry -> entry.getKey().equals(id))
+                .map(entry -> {
+                    var album = new Album();
+                    album.setId(entry.getKey());
+                    album.setTitle(entry.getValue());
+                    return album;
+                }).findAny();
+    }
 
 }
